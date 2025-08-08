b75470a2611bd8715e7eb81ff0ab0e6f47fe4194
Nguyen Nhut Trung <suncore.april@gmail.com>
2025-08-08 15:12:53 +0700

    update

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route('/hanet-webhook', methods=['POST'])
 def hanet_webhook():
-    data = request.json
-    print("Webhook data received:", data)
+    data = None
+    # Cố gắng lấy JSON
+    if request.is_json:
+        data = request.get_json()
+    else:
+        # Nếu không phải JSON, lấy raw data hoặc form
+        data = request.data.decode('utf-8') or request.form.to_dict()
+    print("Webhook data received:")
+    print(data)
     return {"status": "ok"}
 
 if __name__ == '__main__':
