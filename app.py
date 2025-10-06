2725b6200ff381e416f99f47e8602882a651b21a
Nguyen Nhut Trung <suncore.april@gmail.com>
2025-10-06 08:59:42 +0700

    feat: add swagger

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request
+from flasgger import Swagger
+
 
 app = Flask(__name__)
+swagger = Swagger(app, template_file='swagger.yml')
+
 
 @app.route('/hanet-webhook', methods=['POST'])
 def hanet_webhook():
     data = None
-    # Cố gắng lấy JSON
+
     if request.is_json:
         data = request.get_json()
     else:
-        # Nếu không phải JSON, lấy raw data hoặc form
         data = request.data.decode('utf-8') or request.form.to_dict()
+
     print("Webhook data received:")
     print(data)
     return {"status": "ok"}
